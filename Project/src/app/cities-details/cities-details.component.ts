4f237a833d8841e32196eebe6d3d6660f1be6a63
DonatellaTesei <donatella.tesei@gmail.com>
2023-04-17 16:41:18 +0200

    Home and About page changed

---
+++ Project/src/app/cities-details/cities-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, VERSION, ViewEncapsulation } from "@angular/core";
+import lgZoom from 'lightgallery/plugins/zoom';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
+@Component({
+  selector: 'app-cities-details',
+  templateUrl: './cities-details.component.html',
+  styleUrls: ['./cities-details.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CitiesDetailsComponent {
+  name = "Angular " + VERSION.major;
+  settings = {
+    counter: false,
+    plugins: [lgZoom]
+  };
+  onBeforeSlide = (detail: BeforeSlideDetail): void => {
+    const { index, prevIndex } = detail;
+    console.log(index, prevIndex);
+  };
+  
+}
